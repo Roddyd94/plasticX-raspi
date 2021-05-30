28ed3a0e1e19b22fe76ff2f8b6816b481e1f1b30
Roddyd94 <pky941129@gmail.com>
2021-05-30 16:06:59 +0900

    blah blah

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
+import threading
 from gpiozero import SmoothedInputDevice
 
-sensor_ir = SmoothedInputDevice(2)
 
-while True:
-    print("Sensor:", sensor_ir.value())
-    qr_text = input()
-    print("QR:", qr_text)
+class IRSensor(SmoothedInputDevice):
+    def __init__(self, pin, *args, **kwargs):
+        super().__init__(pin, *args, **kwargs)
+
+    def run(self):
+        while True:
+            print("Sensor:", self.value())
+
+
+def run_qr():
+    while True:
+        qr_text = input()
+        print("QR:", qr_text)
+
+
+class SensorRunner(threading.Thread):
+    def __init__(self, run):
+        super().__init__()
+        self.run = run
+
+
+ir_sensor = IRSensor(4)
+
+SensorRunner(ir_sensor.run)
+SensorRunner(run_qr)
