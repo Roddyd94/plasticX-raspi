8ae08962da09b9807800d3847081733ada6f7720
Roddyd94 <pky941129@gmail.com>
2021-05-30 14:55:42 +0900

    added sensor

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from gpiozero import SmoothedInputDevice
+
+sensor_ir = SmoothedInputDevice(2)
+
 while True:
+    print("Sensor:", sensor_ir.value())
     qr_text = input()
-    print("QR: ", qr_text)
+    print("QR:", qr_text)
