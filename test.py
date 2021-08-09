55a166777875db0a2e3ff6220e435a5399ddc481
Roddyd94 <pky941129@gmail.com>
2021-08-09 15:09:44 +0900

    buzzer changed

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import threading
+import sys
 import time
 from urllib import request
 from urllib import parse
 #from urllib import response
+import json
 import env
 import smbus
-from gpiozero import LightSensor
-
+from gpiozero import TonalBuzzer
+from gpiozero.tones import Tone
 
 class QRThread(threading.Thread):
     def __init__(self, config):
@@ -14,6 +16,7 @@ class QRThread(threading.Thread):
         self.id = config.id if config else env.id
         self.baseUrl = config.baseUrl if config else env.baseUrl
         self.path = config.pathTumbler if config else env.pathTumbler
+        self.buzzer = TonalBuzzer(env.pin_buzz)
 
     def run(self):
         while True:
@@ -22,12 +25,36 @@ class QRThread(threading.Thread):
 
     def send(self, id='60d2c06885142b1264c81cb7'):
         print(id)
-        data = parse.urlencode({'to_id': self.id, 'state': 'false'})
+        data = parse.urlencode({'to_id': self.id})
         req = request.Request(f'{self.baseUrl}{self.path}{id}', data=bytes(data, 'UTF-8'), method='PUT')
-        with request.urlopen(req) as response:
-            res = response.read()
-            print(res)
-
+        try:
+            with request.urlopen(req) as response:
+                res = json.loads(response.read().decode('utf-8'))
+                if res['RESULT'] == 401:
+                    print('Invalid QR Code')
+                    self.buzzer.play(Tone("G4"))
+                    time.sleep(0.4)
+                    self.buzzer.stop()
+                    time.sleep(0.2)
+                    self.buzzer.play(Tone("G4"))
+                    time.sleep(0.4)
+                    self.buzzer.stop()
+                elif res['RESULT'] == 200:
+                    print('Tumbler Returned')
+                    self.buzzer.play(Tone("C5"))
+                    time.sleep(0.2)
+                    self.buzzer.play(Tone("E5"))
+                    time.sleep(0.2)
+                    self.buzzer.play(Tone("G5"))
+                    time.sleep(0.4)
+                    self.buzzer.stop()
+                else:
+                    print(res)
+                    self.buzzer.play(Tone("G4"))
+                    time.sleep(1)
+                    self.buzzer.stop()
+        except:
+            print(sys.exc_info()[0])
 
 class I2CThread(threading.Thread):
     def __init__(self, config):
@@ -38,6 +65,7 @@ class I2CThread(threading.Thread):
         self.addr = config.addr if config else env.addr
         self.bus = smbus.SMBus(config.bus if config else env.bus)
         self.buf = [0,0,0,0,0,0,0,0,0,0]
+        self.state = False
 
     def run(self):
         while True:
@@ -45,9 +73,10 @@ class I2CThread(threading.Thread):
                 self.bus.write_i2c_block_data(self.addr, 0, [0x40])
                 self.buf[i] = self.bus.read_i2c_block_data(self.addr, 0, 2)[1]
             adc = sum(self.buf) / len(self.buf)
-            if adc > 110:
+            print(adc)
+            if adc < 90:
                 self.send(False)
-            elif adc < 100:
+            elif adc > 110:
                 self.send(True)
             time.sleep(10)
 
@@ -55,9 +84,12 @@ class I2CThread(threading.Thread):
         print(f'ReturnBox {self.id} is ' + ('full' if is_full else 'not full'))
         data = parse.urlencode({'isFull': 'true' if is_full else 'false', 'isWorking': 'true'})
         req = request.Request(f'{self.baseUrl}{self.path}{self.id}', data=bytes(data, 'UTF-8'), method='PUT')
-        with request.urlopen(req) as response:
-            res = response.read()
-            print(res)
+        try:
+            with request.urlopen(req) as response:
+                res = json.loads(response.read().decode('utf-8'))
+                print(res)
+        except:
+            print(sys.exc_info()[0])
 
 
 def main():
